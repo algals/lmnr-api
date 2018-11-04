0fb08413912e0e4bc3ec8b4d24d3a4b91c27da33
angliukas <algirdas.alisauskas@gmail.com>
2018-11-04 23:27:28 +0200

    up

--- src/main/java/lt/luminor/lmnrapi/utils/ContactsCreator.java
+++ src/main/java/lt/luminor/lmnrapi/utils/ContactsCreator.java
@@ -21,7 +21,7 @@ public class ContactsCreator {
         Arrays.asList(
                 new ContactEntity("a6edfe4f-fdce-48fd-b2dd-a1ba08d339f1","algals", "algals@alg.lt", "+37068238512", "Pilies5, Vilnius"),
                 new ContactEntity("1d677424-1bbb-4e50-ab40-86667603dd8f","petras", "petras@petras.lt", "+37067378233", "Nemuno 78, Kaunas"),
-                new ContactEntity("456","oid", "email", "phone", "address")
+                new ContactEntity("3d0f0406-a6ec-4b53-8b31-4525b23c9d2d","jonas", "jonas@jonas.lt", "+37965789666", "Kranto 8, Klaipeda")
         ).stream().forEach(x -> contactRepository.save(x));
     }
 
